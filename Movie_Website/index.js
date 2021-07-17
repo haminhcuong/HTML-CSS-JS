a9a742737e74747ad8d7e597a27ba72be20d6596
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2021-07-17 13:48:58 +0700

    light-mode and responsive Movie Website

--- Movie_Website/index.js
+++ Movie_Website/index.js
@@ -1,8 +1,22 @@
 const toggleBall = document.querySelector(".toggle-ball");
 const menuListItems = document.querySelectorAll(".menu-list-item");
+const toggle = document.querySelector(".toggle");
+const navbar = document.querySelector(".navbar");
+const navbarContainer = document.querySelector(".navbar-container");
+const sidebar = document.querySelector(".sidebar");
+const sidebarIcons = document.querySelectorAll(".sidebar-icon");
+const container = document.querySelector(".container");
 
 toggleBall.addEventListener("click", function (e) {
-  toggleBall.classList.toggle("active");
+  toggleBall.classList.toggle("light");
+  toggle.classList.toggle("light");
+  navbar.classList.toggle("light");
+  navbarContainer.classList.toggle("light");
+  sidebar.classList.toggle("light");
+  sidebarIcons.forEach((sidebarIcon) => {
+    sidebarIcon.classList.toggle("light");
+  });
+  container.classList.toggle("light");
 });
 
 menuListItems.forEach((menuListItem) => {
